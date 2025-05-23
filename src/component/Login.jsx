2c73eb536072cf4405764d7c9491a980e7c8a350
alishaikh9890 <alishanshaikh9890@gmail.com>
2025-05-23 16:58:45 +0530

    on auth state change

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import app from '../firebase'
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, updateProfile, GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, updateProfile, GoogleAuthProvider, GithubAuthProvider, onAuthStateChanged, setPersistence, browserLocalPersistence, browserSessionPersistence, inMemoryPersistence } from 'firebase/auth'
 import { toast } from 'react-toastify'
 import Navbar from './Navbar'
 
 
 const Login = () => {
 
-  const [url, setUrl] = useState("")
+const [remember, setRemember] =useState(false)
+  const [user, setUser] = useState({photo:"", name:"", email:""})
+
 
 
     const auth = getAuth(app)
@@ -27,15 +29,36 @@ const Login = () => {
     }
 
 
+
+  
+  
+
+
+    useEffect(() => {
+
+        const persisteProvider =  remember  ? browserLocalPersistence : browserSessionPersistence 
+
+     setPersistence(auth, persisteProvider)
+
+       onAuthStateChanged(auth, (user)=>{
+        if(user)
+        {
+          console.log(user)
+           
+          setUser({...user, photo:user.photoURL, name:user.displayName, email:user.email})
+        }
+       })
+    },[auth])
+
     const handleGoogle = async () => {
       
       try {
  
         let res =  await signInWithPopup(auth, googleProvider);
-          console.log(auth.currentUser)
+          // console.log(auth.currentUser)
           toast("goolge Signin  Successfull...!")
 
-          setUrl(auth.currentUser.photoURL)
+          // setUrl(auth.currentUser.photoURL)
       } catch (error) {
         toast("goolge Signin  fiale3d...!")
         console.log(error.code)
@@ -45,9 +68,9 @@ const Login = () => {
     const handleGithub = async () => {
        try {
         let res = await signInWithPopup(auth, githubProvider) 
-          console.log(auth.currentUser)
-            toast("Github Signin  Successfull...!")
-             setUrl(auth.currentUser.photoURL)
+          // console.log(auth.currentUser)
+            // toast("Github Signin  Successfull...!")
+             
         } catch (error) {
           toast("Github Signin  fiale3d...!")
           console.log(error.code)
@@ -99,7 +122,7 @@ const Login = () => {
 
   return (
     <div>
-         <Navbar photo={url} />
+         <Navbar user={user} />
         <div className="container">
             <div className="row">
                 <div className="col-4">
@@ -120,6 +143,10 @@ const Login = () => {
     <input type="checkbox" className="form-check-input" id="exampleCheck1">
     <label className="form-check-label" for="exampleCheck1">Check me out</label>
   </div> */}
+<div className='d-flex gap-2 justify-content-start pb-4'>
+  <input type="checkbox" className='form-check-input' checked={remember} onChange={() => setRemember(!remember)} />
+  <label className={remember ? 'text-success' : "text-dark"}>Remember Me </label>
+</div>
   <button onClick={handleReg} className="btn btn-primary">Register</button>
   <button onClick={handleLog} className="btn btn-primary">LOgin</button>
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,6 +1,17 @@
+import { signOut, getAuth } from 'firebase/auth'
 import React from 'react'
+import app from '../firebase'
 
-const Navbar = ({photo}) => {
+const Navbar = ({user}) => {
+
+  const auth = getAuth(app)
+
+const logout  = async () => {
+  await signOut(auth);
+
+}
+
+
   return (
     <>
     <nav class="bg-gray-800">
@@ -48,15 +59,15 @@ const Navbar = ({photo}) => {
             <button type="button" class="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
               <span class="absolute -inset-1.5"></span>
               <span class="sr-only">Open user menu</span>
-              <img class="size-8 rounded-full" src={photo} alt=""/>
+              <img class="size-8 rounded-full" src={user.photo} alt=""/>
             </button>
           </div>
 
           <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
          
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">{user.name}</a>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">{user.email}</a>
+            <button onClick={() => logout()} class="block px-4 py-2 text-sm text-gray-700 btn btn-danger" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</button>
           </div>
         </div>
       </div>
